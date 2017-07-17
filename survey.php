ff3cbdb79264b5149bdce62312f02a555d1956c9
Vic <vcao@findsomewinmore.com>
2017-07-17 13:14:20 -0400

    Addition of ONPU Memorial Survey

---
+++ survey.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Template Name: Survey
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages and that
+ * other 'pages' on your WordPress site will use a different template.
+ *
+ * @link http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Smores
+ * @since Smores 2.0
+ */
+?>
+
+    <?php get_template_part('templates/header'); ?>
+
+
+
+
+<div class="main-wrapper">
+
+<div class="page-main">
+
+
+<section>
+    <div class="container pb64">
+
+
+            <?php
+
+  if ( is_user_logged_in())  { ?>
+
+
+        <h1><span>onePULSE Foundation</span>Memorial Survey</h1>
+
+        <a href="#english" class="btn survey-toggle">Continue English</a>
+        <a href="#spanish" class="btn survey-toggle" >Continue Spanish</a>
+
+        <div id="english">
+
+        <?php echo do_shortcode('[gravityform id="1" title="false" description="false" ajax="true"]');?>
+        </div>
+        <div id="spanish">
+
+        <?php echo do_shortcode('[gravityform id="2" title="false" description="false" ajax="true"]');?>
+        </div>
+
+        <?php the_content();?>
+
+<?php } else { ?>
+
+        <h1>Please Sign in to Complete Survey</h1>
+
+        <div class="survey-login">
+                <?php wp_login_form(); ?>
+</div>
+
+
+<?php }?>
+
+    </div>
+
+</section>
+</div>
+
+
+        <?php get_template_part('templates/footer'); ?>
